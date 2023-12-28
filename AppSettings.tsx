514eb043f335e40d79f24b7298e706c90a139421
Tomas Mikulaj <mikulajt@gmail.com>
2023-12-28 11:20:54 +0100

    Add synchronization of all traffic route events

--- AppSettings.tsx
+++ AppSettings.tsx
@@ -1,10 +1,28 @@
-import { Text, View } from "react-native";
+import { ToastAndroid, View } from "react-native";
 import GlobalStyles from "./GlobalStyles";
+import IconButton from "./IconButton";
 
 function AppSettings() {
+
+  async function syncAllRouteEvents() {
+    try {
+      ToastAndroid.show("Začala synchronizace dopravních událostí", ToastAndroid.LONG);
+      const response = await fetch(`http://192.168.88.7:7246/api/trafficRoutes/events/sync`, {
+        method: "POST"
+      });
+      if (response.ok) {
+        ToastAndroid.show("Synchronizace dopravních událostí byla dokončena", ToastAndroid.LONG);
+      } else {
+        ToastAndroid.show("Nastala chyba během synchronizace dopravních událostí", ToastAndroid.LONG);
+      }
+    } catch (error) {
+      ToastAndroid.show("Nastala chyba během synchronizace dopravních událostí", ToastAndroid.LONG);
+    }
+  }
+
   return (
     <View style={GlobalStyles.viewContainer}>
-      <Text>TODO</Text>
+      <IconButton icon="refresh" text="Aktualizovat dopravní události" onPress={syncAllRouteEvents}></IconButton>
     </View>
   );
 }
