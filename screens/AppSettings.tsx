4f080a7d53cae8194020e17f658be15abe8d064c
Tomas Mikulaj <mikulajt@gmail.com>
2024-05-18 22:56:40 +0200

    Remove sync of all route events from settings

--- screens/AppSettings.tsx
+++ screens/AppSettings.tsx
@@ -1,27 +1,7 @@
-import { ToastAndroid, View } from "react-native";
-import GlobalStyles from "../assets/GlobalStyles";
-import IconButton from "../components/IconButton";
-import RouteEventsRequest from "../api/RouteEventsRequests";
-import Config from "react-native-config";
-
 function AppSettings() {
-  const routeEventsRequests = new RouteEventsRequest(`${Config.TEI_API_KEY}/trafficRoutes`);
-
-  async function syncAllRouteEvents() {
-    ToastAndroid.show("Začala synchronizace dopravních událostí", ToastAndroid.LONG);
-    const response = await routeEventsRequests.syncAllRouteEvents();
-    if (response.success) {
-      ToastAndroid.show("Synchronizace dopravních událostí byla dokončena", ToastAndroid.LONG);
-    }
-    else {
-      ToastAndroid.show("Nastala chyba během synchronizace dopravních událostí", ToastAndroid.LONG);
-    }
-  }
 
   return (
-    <View style={GlobalStyles.viewContainer}>
-      <IconButton icon="refresh" text="Aktualizovat dopravní události" onPress={syncAllRouteEvents}></IconButton>
-    </View>
+    <></>
   );
 }
 
