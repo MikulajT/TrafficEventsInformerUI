fb65d3f8252a25eed6fd30a5eb6613a9d8e4cafa
Tomas Mikulaj <mikulajt@gmail.com>
2024-05-28 19:45:37 +0200

    Add time picker (currently unused)

--- screens/AppSettings.tsx
+++ screens/AppSettings.tsx
@@ -1,6 +1,30 @@
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import { Button } from 'react-native-paper';
+
 function AppSettings() {
+  const [date, setDate] = useState(new Date(1598051730000));
+
+
+  const showMode = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      mode: "time",
+      is24Hour: true,
+    });
+  };
+
+  const showTimepicker = () => {
+    showMode();
+  };
 
   return (
+    // <SafeAreaView>
+    //   <Button onPress={showTimepicker}>
+    //     Show time picker
+    //   </Button>
+    // </SafeAreaView>
     <></>
   );
 }
